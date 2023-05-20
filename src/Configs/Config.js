68708441f2c0bf1c66609521610a78ba5ed714f4
Glitch (just-actually-breath) <none>
2023-05-20 13:53:41 +0000

    🏰🚈 Checkpoint ./src/Configs/Config.js:49830958/844

--- src/Configs/Config.js
+++ src/Configs/Config.js
@@ -1,6 +1,6 @@
 module.exports = {
     Token: "",
-    MongoDB: "mongodb+srv://elcnakoz123_:<elcnakoz123_>@cluster000.2akddoj.mongodb.net/?retryWrites=true&w=majority",
+    MongoDB: "mongodb+srv://elcnakoz123_:<elcnakoz123_>@cluster0sa.y0lonis.mongodb.net/",
     Owners: ['768757288940208138', ''],
     Prefix: ["!"],
 
